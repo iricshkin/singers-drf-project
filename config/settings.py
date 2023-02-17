3179ba0f5ce4b2ffbc8a5e9e8d53b9be4b6c0251
Irina Fok <fokira@mail.ru>
2023-02-17 22:37:03 +0300

    Добавлены drf и drf_yasg

--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'drf_yasg',
     'singers.apps.SingersConfig',
 ]
 
