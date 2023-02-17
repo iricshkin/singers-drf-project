0effe45e151716ff725bf52bff1a00e8338e5f37
Irina Fok <fokira@mail.ru>
2023-02-17 22:04:26 +0300

    Добавлена админка

--- singers/admin.py
+++ singers/admin.py
@@ -1,3 +1,26 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from .models import Album, Singer, Song, SongInAlbum
+
+
+class SongInAlbumInline(admin.TabularInline):
+    model = SongInAlbum
+
+
+class AlbumAdmin(admin.ModelAdmin):
+    inlines = (SongInAlbumInline,)
+    list_display = ('id', 'title', 'singer', 'year')
+    search_fields = ('title', 'singer')
+    list_filter = ('singer',)
+
+
+class SongAdmin(admin.ModelAdmin):
+    inlines = (SongInAlbumInline,)
+    exclude = ('album',)
+    search_fields = ('song',)
+
+
+admin.site.register(Song, SongAdmin)
+admin.site.register(SongInAlbum)
+admin.site.register(Singer)
+admin.site.register(Album, AlbumAdmin)
--- singers/validators.py
+++ singers/validators.py
@@ -8,4 +8,4 @@ def validate_year(year):
 
     current_year = datetime.datetime.now()
     if year < 1900 or year > current_year.year:
-        raise ValidationError(f'Некорректный год {current_year.year}')
+        raise ValidationError('Указан екорректный год!')
