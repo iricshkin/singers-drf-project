74174d4dfede9f6f6ace9256555e6c8788aa72d8
Irina Fok <fokira@mail.ru>
2023-02-20 20:55:22 +0300

    Убран дебаг в config

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY', default='123')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 DEBUG = bool(int(os.getenv('DEBUG', default=1)))
 
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS').split(' ')
